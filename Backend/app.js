6c70e93ec88e247cf23bc7961ead04dd1e891244
Astha Chaurasia <asthagithub@gmail.com>
2026-04-08 10:04:27 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -16,8 +16,8 @@ import morgan from 'morgan';
 const app = express();
 
 app.use(cors({
-  // origin: process.env.FRONTEND_URL, 
-  origin: "*", 
+  origin: process.env.FRONTEND_URL, 
+ 
   
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
