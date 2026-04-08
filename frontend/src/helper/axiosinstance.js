061507510b689237747b213da908b8ebe3428a82
Astha Chaurasia <asthagithub@gmail.com>
2026-04-08 08:36:36 +0530

    Update axiosinstance.js

--- frontend/src/helper/axiosinstance.js
+++ frontend/src/helper/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const BASE_URL="http://localhost:5000/api/v1";
+const BASE_URL="https://surakshasetu-t3n3.onrender.com/api/v1";
 
 
 const axiosInstance=axios.create();
